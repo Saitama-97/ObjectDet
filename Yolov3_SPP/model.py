1a8c2641824fd66a553d042f5a8ee8299dbbddde
Saitama <2822863494@qq.com>
2024-01-10 15:30:00 +0800

    Yolov3_SPP README

---
+++ Yolov3_SPP/model.py
@@ -0,0 +1,10 @@
+# _*_ coding: utf-8 _*_
+
+"""
+  @Time    : 2024/1/10 15:15 
+  @File    : model.py
+  @Project : ObjectDet
+  @Author  : Saitama
+  @IDE     : PyCharm
+  @Des     : Yolov3_SPP 模型
+"""
